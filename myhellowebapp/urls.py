bcf1a1f0d07fd468d295a757b5beb0a556ec5d07
carstenpleiser <carsten.pleiser@gmx.de>
2016-04-09 13:01:43 +0100

    Added 2 step registration and limited that only owners of an experience can edit.

--- myhellowebapp/urls.py
+++ myhellowebapp/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from tpe import views
-from django.contrib.auth.views import (password_reset, password_reset_done, password_reset_confirm, password_reset_complete)
+from django.contrib.auth.views import (password_reset, password_reset_done, password_reset_confirm, password_reset_complete, password_change, password_change_done)
 from tpe.backends import MyRegistrationView
 
 urlpatterns = [
@@ -30,6 +30,8 @@ urlpatterns = [
     url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name':'registration/password_reset_done.html'}, name= "password_reset_done"),
     url(r'^accounts/password/reset/(&P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name':'registration/password_reset_confirm.html'}, name= "password_reset_confirm"),
     url(r'^accounts/password/done/$', password_reset_complete, {'template_name':'registration/password_reset_complete.html'}, name= "password_reset_complete"),
+    url(r'^accounts/password/change/$', password_change, { 'template_name': 'registration/password_change_form.html'}, name='password_change'),
+    url(r'^accounts/password/change/done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}, name='password_change_done'),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/create_experience/$', views.create_experience, name='registration_create_experience'),
     url(r'^accounts/', include('registration.backends.default.urls')),
