f939152968ebc4f3ce4182bed97aba73643f16d6
carstenpleiser <carsten.pleiser@gmx.de>
2016-04-09 16:45:51 +0100

    Pre Deployment

--- myhellowebapp/settings.py
+++ myhellowebapp/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../myhellowebapp/static'),)
 
 ACCOUNT_ACTIVATION_DAYS = 7
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
